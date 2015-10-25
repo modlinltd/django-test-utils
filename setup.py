3bf583b90eed0d7194d11608c63496c0ca561f14
Pavel Savchenko <pavel@modlinltd.com>
2015-10-25 09:39:51 +0100

    chore: use a unique version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-test-utils",
-    version = "0.3",
+    version = "0.3.1git",
     packages = find_packages(),
     author = "Eric Holscher",
     author_email = "eric@ericholscher.com",
